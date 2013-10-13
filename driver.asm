646df171482e5226e00280fa2ba9721239ad2032
Kacey <cole.kacey@gmail.com>
2013-10-13 14:03:16 -0700

    Assembly drivers

---
+++ driver.asm
@@ -0,0 +1,266 @@
+; declare variables
+a1 	.INT 	1
+a2	.INT	2
+a3	.INT	3
+a4	.INT	4
+a5	.INT	5
+a6	.INT	6
+b1	.INT 	300
+b2	.INT	150
+b3	.INT	50
+b4	.INT	20
+b5	.INT	10
+b6	.INT	5
+c1	.INT	500
+c2	.INT	2
+c3	.INT	5
+c4	.INT	10
+; Name declaration
+n1 		.BYT	'C'
+n2 		.BYT	'o'
+n3 		.BYT	'l'
+n4 		.BYT	'e'
+n5 		.BYT	','
+n6 		.BYT	' '
+n7	 	.BYT	'K'
+n8	 	.BYT	'a'
+n9	 	.BYT	'c'
+n10 	.BYT	'e'
+n11 	.BYT	'y'
+; Extras
+line	.BYT	'\n'
+blank	.BYT	' '
+
+; Driver
+;print name
+LDB	r1	n1
+TRP	3
+LDB	r1	n2
+TRP	3
+LDB	r1	n3
+TRP	3
+LDB	r1	n4
+TRP	3
+LDB	r1	n5
+TRP	3
+LDB	r1	n6
+TRP	3
+LDB	r1	n7
+TRP	3
+LDB	r1	n8
+TRP	3
+LDB	r1	n9
+TRP	3
+LDB	r1	n10
+TRP	3
+LDB	r1	n11
+TRP	3
+LDB	r1	line
+TRP	3
+;print blank line
+TRP	3
+
+;add all elements of b
+;	print each step, place 2 spaces between each print.
+LDR	r2	b1
+LDR	r3	b2
+ADD	r2	r3
+MOV r1	r2
+TRP	1	; print value
+LDB	r1	blank
+TRP	3	; space
+TRP	3	; space
+
+LDR	r3	b3
+ADD	r2	r3
+MOV r1	r2
+TRP	1
+LDB	r1	blank
+TRP	3
+TRP	3
+
+LDR	r3	b4
+ADD	r2	r3
+MOV r1	r2
+TRP	1
+LDB	r1	blank
+TRP	3
+TRP	3
+
+LDR	r3	b5
+ADD	r2	r3
+MOV r1	r2
+TRP	1
+LDB	r1	blank
+TRP	3
+TRP	3
+
+LDR	r3	b6
+ADD	r2	r3
+MOV r1	r2
+TRP	1
+;Save this value for later
+MOV	r5	r2
+
+LDB	r1	line	;end the line
+TRP	3
+;print blank line
+TRP 3
+
+;Multiply all elements of a
+;	print each step, place 2 spaces between each print.
+LDR	r2	a1
+LDR	r3	a2
+MUL	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+LDR	r3	a3
+MUL	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+LDR	r3	a4
+MUL	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+LDR	r3	a5
+MUL	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+LDR	r3	a6
+MUL	r2	r3
+MOV	r1	r2
+TRP	1
+; Save value for later
+MOV	r6	r2
+
+LDB	r1	line	;end the line
+TRP	3
+;print blank line
+TRP 3
+
+;Divide the final result from sum(b) by each element in b - non cumulative.
+;	print each result, place 2 spaces between each print.
+MOV r2	r5
+LDR	r3	b1
+DIV	r2	r3
+MOV	r2	r1
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+MOV r2	r5
+LDR	r3	b2
+DIV	r2	r3
+MOV	r2	r1
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+MOV r2	r5
+LDR	r3	b3
+DIV	r2	r3
+MOV	r2	r1
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+MOV r2	r5
+LDR	r3	b4
+DIV	r2	r3
+MOV	r2	r1
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+MOV r2	r5
+LDR	r3	b5
+DIV	r2	r3
+MOV	r2	r1
+TRP	1
+LDB	r1	blank
+TRP 3
+TRP 3
+
+MOV r2	r5
+LDR	r3	b6
+DIV	r2	r3
+MOV	r2	r1
+TRP	1
+
+LDB	r1	line	;end the line
+TRP	3
+;print blank line
+TRP 3
+
+;Subtract from the final result of mul(a) each element of c - non cumulative.
+;	print each result, place 2 spaces between each print.
+MOV r2	r6
+LDR	r3	a1
+SUB	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP	3
+TRP	3
+
+MOV r2	r6
+LDR	r3	a2
+SUB	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP	3
+TRP	3
+
+MOV r2	r6
+LDR	r3	a3
+SUB	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP	3
+TRP	3
+
+MOV r2	r6
+LDR	r3	a4
+SUB	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP	3
+TRP	3
+
+MOV r2	r6
+LDR	r3	a5
+SUB	r2	r3
+MOV	r1	r2
+TRP	1
+LDB	r1	blank
+TRP	3
+TRP	3
+
+MOV r2	r6
+LDR	r3	a6
+SUB	r2	r3
+MOV	r1	r2
+TRP	1
---
+++ test.asm
@@ -0,0 +1,8 @@
+.int 1
+.int -1
+foo .int -2
+.byt 'a'
+bar .byt 'n'
+
+LDR r1 foo
+ADD r1 #2
