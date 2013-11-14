a79b9efab86c7e5a55c4ec615d532d66ca3bbade
Kacey <cole.kacey@gmail.com>
2013-11-14 15:30:37 -0800

    Working project 3

--- proj3.asm
+++ proj3.asm
@@ -144,30 +144,80 @@ mwh1    LDA     r9  c
         LDB     r0  at
         CMP     r0  r1
         BRZ     r0  mewh1
-mif1    LDB     r0  pos  ; if (c[0] == '+') || c[0] == '-')
-        CMP     r0  r1
-        BRZ     mif1T
-        LDB     r0  neg
-        CMP     r0  r1
-        BRZ     mif1T    ; else
-        MOV     r8  r9
-        ADI     r8  #1
-        STB     r1  r8   ; c[1] = c[0]
-        LDB     r0  pos
-        STB     r0  r9   ; c[0] = '+'
-        LDR     r0  cnt
-        ADI     r0  #1   ; cnt++
-        STR     r0  cnt
-meif1   MOV     r1  r1   ; waste cycle
-mwh2    LDR     r1  data ; while (data)
-mif2    LDA     r9  c
-        LDR     r8  cnt
-        ADI     r8  #-1
-        LDB     r0  r8
-        LDB     r1  line
-        CMP     r0  r1
-        BNZ     r0  mif2T  ; if (c[cnt-1] == '\n')
-        ; else getdata()
+    mif1    LDB     r0  pos  ; if (c[0] == '+') || c[0] == '-')
+            CMP     r0  r1
+            BRZ     r0  mif1T
+            LDB     r0  neg
+            CMP     r0  r1
+            BRZ     r0  mif1T    ; else
+                MOV     r8  r9
+                ADI     r8  #1
+                STB     r1  r8   ; c[1] = c[0]
+                LDB     r0  pos
+                STB     r0  r9   ; c[0] = '+'
+                LDR     r0  cnt
+                ADI     r0  #1   ; cnt++
+                STR     r0  cnt
+    meif1   MOV     r1  r1   ; waste cycle
+    mwh2    LDR     r1  data ; while (data)
+            LDR     r0  zero
+            CMP     r0  r1
+            BRZ     r0  mewh2
+        mif2    LDA     r9  c
+                LDR     r8  cnt
+                ADI     r8  #-1
+                MOV     r7  r9
+                ADD     r7  r8
+                LDB     r0  r7
+                LDB     r1  line
+                CMP     r0  r1
+                BRZ     r0  mif2T  ; if (c[cnt-1] == '\n')
+                ; else getdata()
+                    MOV     r5  sp  ; Calculate record size
+                    ADI     r5  #-8
+                    CMP     r5  st  ; Check for overflow
+                    BLT     r5  odie
+                    MOV     r6  fp  ; temp for PFP
+                    MOV     fp  sp  ; set FP to next
+                    ADI     sp  #-8 ; Leave space for return addy
+                    STR     r6  sp  ; save pfp
+                    MOV     r5  pc  ; Calculate return addy
+                    ADI     r5  #60 ; Increment to line after jmp
+                    ADI     sp  #4  ; back to save return addy
+                    STR     r5  sp
+                    ADI     sp  #-4
+                    JMP     getdata
+        meif2   MOV     r1  r1  ; endif 2
+        JMP     mwh2
+mewh2   MOV     r1 r1  ; endwhile
+        ; reset(1, 0, 0, 0) // Reset globals
+        MOV     r5  sp   ; Calculate record size
+        ADI     r5  #-24 ; 4 ints, pfp, return add
+        CMP     r5  st   ; Check for overflow
+        BLT     r5  odie
+        MOV     r6  fp   ; temp for PFP
+        MOV     fp  sp   ; set FP to next
+        ADI     sp  #-8  ; Leave space for return addy
+        STR     r6  sp   ; save pfp
+        LDR     r0  zero
+        ADI     r0  #1
+        ADI     sp  #-4
+        STR     r0  sp   ; arg 1 = 1
+        LDR     r0  zero
+        ADI     sp  #-4
+        STR     r0  sp   ; arg 2 = 0
+        ADI     sp  #-4
+        STR     r0  sp   ; arg 3 = 0
+        ADI     sp  #-4
+        STR     r0  sp   ; arg 4 = 0
+        MOV     r5  pc   ; Calculate return addy
+        ADI     r5  #60  ; Increment to line after jmp
+        ADI     sp  #20  ; back to save return addy
+        STR     r5  sp
+        ADI     sp  #-20
+        JMP     reset
+
+        ; getdata()
         MOV     r5  sp  ; Calculate record size
         ADI     r5  #-8
         CMP     r5  st  ; Check for overflow
@@ -182,11 +232,7 @@ mif2    LDA     r9  c
         STR     r5  sp
         ADI     sp  #-4
         JMP     getdata
-        meif2    MOV     r1  r1  ; endif 2
-                      ; endif
-            ; endwhile
-            ; reset(1, 0, 0, 0)
-            ; getdata()
+
         JMP    mwh1
 mewh1   JMP    retmain  ; endwhile
 
@@ -224,7 +270,36 @@ mwh3    LDR     r1  zero ; while (!flag && cnt != 0)
         LDR     r0  cnt
         CMP     r0  r1
         BRZ     r0  mewh3
-            ; opd(c[0], tenth, c[cnt])
+
+        ; opd(c[0], tenth, c[cnt])
+        MOV     r5  sp
+        ADI     r5  #-14   ; 2 char, 1 int, pfp, return add
+        CMP     r5  st     ; check for overflow
+        BLT     r5  odie
+        MOV     r6  fp     ; temp for PFP
+        MOV     fp  sp     ; set FP to next
+        ADI     sp  #-8    ; Leave space for return addy
+        STR     r6  sp     ; save pfp
+        ; Fetch args
+        LDA     r9  c
+        LDB     r0  r9
+        ADI     sp  #-1
+        STB     r0  sp     ; arg 1 = c[0]
+        LDR     r0  tenth
+        ADI     sp  #-4
+        STR     r0  sp     ; arg 2 = tenth
+        LDR     r8  cnt
+        ADD     r8  r9
+        LDB     r0  r8
+        ADI     sp  #-1
+        STB     r0  sp     ; arg 3 = c[cnt]
+        MOV     r5  pc     ; Calculate return addy
+        ADI     r5  #60      ; Increment to line after jmp
+        ADI     sp  #10    ; back to save return addy
+        STR     r5  sp
+        ADI     sp  #-10
+        JMP     opd
+
         LDR     r0  cnt
         ADI     r0  #-1
         STR     r0  cnt   ; cnt--
@@ -238,12 +313,47 @@ mewh3   LDR     r1  zero ; endwhile
         LDR     r0  flag
         CMP     r0  r1   ; if (!flag)
         BNZ     r0  meif3
-            ;printf("Operand is %d\n", opdv)
+        LDA     r1  opis ; printf("Operand is %d\n", opdv)
+        LDB     r0  r1
+        TRP     3        ; 'O'
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; 'p'
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; 'e'
+        ADI     r1  #1
+        LDB    r0  r1
+        TRP     3        ; 'r'
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; 'a'
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; 'n'
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; 'd'
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; ' '
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; 'i'
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; 's'
+        ADI     r1  #1
+        LDB     r0  r1
+        TRP     3        ; ' '
+        LDR     r0  opdv
+        TRP     1        ; '%d'
+        LDB     r0  line
+        TRP     3        ; '\n'
+        
 meif3   JMP     meif2
 
-retmain LDB     r0  at
-        TRP     3
-        LDB     r0  line
+retmain LDB     r0  line
         TRP     3
 back    TRP     0
 
@@ -323,7 +433,6 @@ opd     MOV     r5  sp  ; Need to calculate space req's
         ADI     r5  #-4  ; 1 int
         CMP     r5  st  ; Check for overflow
         BLT     r5  odie
-        TRP     99
         LDR     r0  zero
         ADI     sp  #-4
         STR     r0  sp  ; Initialize local t
@@ -331,53 +440,55 @@ opd     MOV     r5  sp  ; Need to calculate space req's
         ADI     sp  #4
         LDB     r0  sp  ; Load for eval
         ADI     sp  #-4 ; go back to local int
+        LDR     r6  sp  ; copy of t for later assignment
+        LDR     r7  zero ; zero out register for adi later
 if1a    MOV     r1  r0  ; Disposable for testing
         LDA     r3  nums
-        LDR     r2  r3 ; fetch '0'
+        LDB     r2  r3 ; fetch '0'
         CMP     r1  r2
         BRZ     r1  case1a ; j == '0'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '1'
+        LDB     r2  r3  ; fetch '1'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1b ; j == '1'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '2'
+        LDB     r2  r3  ; fetch '2'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1c ; j == '2'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '3'
+        LDB     r2  r3  ; fetch '3'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1d ; j == '3'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '4'
+        LDB     r2  r3  ; fetch '4'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1e ; j == '4'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '5'
+        LDB     r2  r3  ; fetch '5'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1f ; j == '5'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '6'
+        LDB     r2  r3  ; fetch '6'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1g ; j == '6'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '7'
+        LDB     r2  r3  ; fetch '7'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1h ; j == '7'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '8'
+        LDB     r2  r3  ; fetch '8'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1i ; j == '8'
         ADI     r3  #1
-        LDR     r2  r3  ; fetch '9'
+        LDB     r2  r3  ; fetch '9'
         MOV     r1  r0
         CMP     r1  r2
         BRZ     r1  case1j ; j == '9'
@@ -438,30 +549,50 @@ else1a  LDA     r1  nan
         STR     r0  flag  ; flag = 1
         JMP     ret1
 ; Time for cases! Ugh
-case1a  TRP     3
-case1b  TRP     3
-case1c  TRP     3
-case1d  TRP     3
-case1e  TRP     3
-case1f  TRP     3
-case1g  TRP     3
-case1h  TRP     3
-case1i  TRP     3
-case1j  TRP     3
+case1a  MOV     r6  r7  ; t = 0
+        JMP     if1b
+case1b  ADI     r7  #1
+        MOV     r6  r7  ; t = 1
+        JMP     if1b
+case1c  ADI     r7  #2
+        MOV     r6  r7  ; t = 2
+        JMP     if1b
+case1d  ADI     r7  #3
+        MOV     r6  r7  ; t = 3
+        JMP     if1b
+case1e  ADI     r7  #4
+        MOV     r6  r7  ; t = 4
+        JMP     if1b
+case1f  ADI     r7  #5
+        MOV     r6  r7  ; t = 5
+        JMP     if1b
+case1g  ADI     r7  #6
+        MOV     r6  r7  ; t = 6
+        JMP     if1b
+case1h  ADI     r7  #7
+        MOV     r6  r7  ; t = 7
+        JMP     if1b
+case1i  ADI     r7  #8
+        MOV     r6  r7  ; t = 8
+        JMP     if1b
+case1j  ADI     r7  #9
+        MOV     r6  r7  ; t = 9
 
-if1b    LDR     r1  flag  ; if !flag
+if1b    STR     r6  sp
+        LDR     r1  flag  ; if !flag
         LDR     r0  zero
         CMP     r0  r1
-        BLT     ret1      ; flag is True
-        LDR     r2  sp    ; fetch t from stack
+        BLT     r0  ret1  ; flag is True
+        MOV     r2  r6    ; fetch t
         ADI     sp  #5
         LDR     r3  sp    ; fetch k from stack
         ADI     sp  #4
-        LDR     r4  sp    ; fetch s from stack
+        LDB     r4  sp    ; fetch s from stack
 if1c    LDB     r0  pos  ; if s == '+'
         CMP     r0  r4
         BRZ     r0  kmul
-kneg    LDR     r0  #-1
+kneg    LDR     r0  zero
+        ADI     r0  #-1
         MUL     r3  r0    ; -k
 kmul    MUL     r2  r3
         LDR     r0  opdv
