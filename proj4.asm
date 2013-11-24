d13764346901a367b7c4b1789cbe2d058bf04c5e
Kacey <cole.kacey@gmail.com>
2013-11-23 16:13:21 -0800

    Project 4 driver initial

---
+++ proj4.asm
@@ -0,0 +1,41 @@
+array   .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0
+        .int    0   ; end of array[30]
+
+; Main
+        TRP     2
+        TRP     1
+        TRP     0
+; End Main
+
+; int fact(int)
+;fact    
+
+; End Fact
