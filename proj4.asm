9ebe761beaf186e98c81af265e21e0ff84596914
Kacey <cole.kacey@gmail.com>
2013-11-23 19:26:00 -0800

    Working recursive factorial.

--- proj4.asm
+++ proj4.asm
@@ -66,7 +66,7 @@ array   .int    0
         .int    0   ; end of array[30]
 
 ; START main
-main    LDA     r1  num
+main    LDA     r1  num ; print 'Enter an int: '
         LDB     r0  r1
         TRP     3
         ADI     r1  #1
@@ -108,13 +108,29 @@ main    LDA     r1  num
         ADI     r1  #1
         LDB     r0  r1
         TRP     3
-        TRP     2
-        TRP     1
+        TRP     2      ; getint()
+        ;TRP     1      ; debugging print value
         ; fact(r0)
         MOV     r5  sp      ; Calculate record size
         ADI     r5  #-12    ; 1 int, pfp, return add
         CMP     r5  st      ; Check for overflow
         BLT     r5  odie
+        MOV     r6  fp      ; temp for PFP
+        MOV     fp  sp      ; set FP to next
+        ADI     sp  #-8     ; Leave space for return addy
+        STR     r6  sp      ; save pfp
+        ADI     sp  #-4
+        STR     r0  sp      ; arg 1 = getint()
+        MOV     r5  pc      ; Calculate return addy
+        ADI     r5  #60     ; Increment to line after jmp
+        ADI     sp  #8      ; back to save return addy
+        STR     r5  sp
+        ADI     sp  #-8
+        JMP     fact
+        ADI     sp  #-4     ; fetch returned value
+        LDR     r0  sp
+        TRP     1
+
 
 end     LDB     r0  line
         TRP     3
@@ -122,7 +138,69 @@ end     LDB     r0  line
 ; END main
 
 ; START int fact(int)
-;fact    
+fact    MOV     r5  sp      ; test for overflow
+        ADI     r5  #-4     ; 1 int
+        CMP     r5  st
+        BLT     r5  odie
+        ;TRP     99
+        LDR     r0  sp      ; fetch n
+        ;TRP     1           ; debugging print
+
+        ; test for n == 0
+        ;TRP     99
+        MOV     r2  r0
+        LDR     r1  zero
+        CMP     r2  r1
+        BRZ     r2  base
+        ; else, n * fact(n - 1)
+        ;TRP     99
+        ADI     sp  #-4     ; make room for n
+        STR     r0  sp      ; store n
+        ; recurse
+        MOV     r5  sp      ; Calculate record size
+        ADI     r5  #-12    ; 1 int, pfp, return add
+        CMP     r5  st      ; Check for overflow
+        BLT     r5  odie
+        MOV     r6  fp      ; temp for PFP
+        MOV     fp  sp      ; set FP to next
+        ADI     sp  #-8     ; Leave space for return addy
+        STR     r6  sp      ; save pfp
+
+        ADI     r0  #-1     ; calculate arg
+        ADI     sp  #-4
+        STR     r0  sp      ; arg 1 = getint()
+        MOV     r5  pc      ; Calculate return addy
+        ADI     r5  #60     ; Increment to line after jmp
+        ADI     sp  #8      ; back to save return addy
+        STR     r5  sp
+        ADI     sp  #-8
+        JMP     fact
+        ; ANDDD we're back. Do the maths.
+        LDR     r1  sp      ; fetch local var n
+        ADI     sp  #-4     ; fetch return
+        LDR     r0  sp
+        ADI     sp  #8      ; move stack to proper location
+        MUL     r1  r0      ; n * returned val
+        JMP     return
+
+        ; set return value as 1
+base    LDR     r1  zero
+        ADI     r1  #1
+
+        ; return sequence
+return  MOV     sp  fp  ; RETURN
+        MOV     r4  sp  ; Test for underflow
+        CMP     r4  sb
+        BGT     r4  udie    ; underflow
+        ADI     sp  #-4
+        LDR     r3  sp  ; return address
+        ADI     sp  #-4
+        LDR     r4  sp  ; retrieve pfp
+        ADI     sp  #4  ; return value
+        STR     r1  sp
+        ADI     sp  #4  ; top of next frame
+        MOV     fp  r4  ; set FP to previous
+        JMR     r3
 
 ; END fact
 
